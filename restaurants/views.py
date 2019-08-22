f6801db8801a731eef5e02431499f35ca73f83b4
Ahmad Qmairah <AhmadQmairah@gmail.com>
2019-08-22 15:04:17 +0300

    Ahmad & Mariam

--- restaurants/views.py
+++ restaurants/views.py
@@ -3,38 +3,50 @@ from .models import Restaurant
 from .forms import RestaurantForm
 
 def welcome(request):
-    return render(request, 'index.html', {'msg':'Hello World!'})
+	return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-    context = {
-        "restaurants":Restaurant.objects.all()
-    }
-    return render(request, 'list.html', context)
+	context = {
+		"restaurants":Restaurant.objects.all()
+	}
+	return render(request, 'list.html', context)
 
 
 def restaurant_detail(request, restaurant_id):
-    context = {
-        "restaurant": Restaurant.objects.get(id=restaurant_id)
-    }
-    return render(request, 'detail.html', context)
+	context = {
+		"restaurant": Restaurant.objects.get(id=restaurant_id)
+	}
+	return render(request, 'detail.html', context)
 
 def restaurant_create(request):
-    form = RestaurantForm()
-    if request.method == "POST":
-        form = RestaurantForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('restaurant-list')
-    context = {
-        "form":form,
-    }
-    return render(request, 'create.html', context)
+	form = RestaurantForm()
+	if request.method == "POST":
+		form = RestaurantForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('restaurant-list')
+	context = {
+		"form":form,
+	}
+	return render(request, 'create.html', context)
 
 def restaurant_update(request, restaurant_id):
-
-    return
+	restaurant=Restaurant.objects.get(id=restaurant_id)
+	
+	form = RestaurantForm(instance=restaurant)
+	if request.method == "POST":
+		form = RestaurantForm(request.POST,instance=restaurant)
+		if form.is_valid():
+			form.save()
+			return redirect('restaurant-list')
+	context = {
+		"form":form,
+		'restaurant':restaurant,
+	}
+	return render(request, 'update.html', context)
 
 def restaurant_delete(request, restaurant_id):
+		
+		Restaurant.objects.get(id=restaurant_id).delete()
 
-
-    return
+		return redirect('restaurant-list')
